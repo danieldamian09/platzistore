c0088155624f9ea225fd078a9982ff6346c9400b
danieldamian09 <humbertorivero9@gmail.com>
2022-03-20 21:56:50 -0300

    create file _document.js for SEO

--- src/components/ProductItem.jsx
+++ src/components/ProductItem.jsx
@@ -15,7 +15,7 @@ const ProductItem = ({ product }) => {
 
   return (
     <div className={styles.ProductItem}>
-      <Image src={product.images[0]} alt={product.title} />
+      {product.images[0] && <Image src={product.images[0]} alt={product.title} width={240} height={240} />}
       <div className={styles['product-info']}>
         <div>
           <p>${product.price}</p>
@@ -23,7 +23,7 @@ const ProductItem = ({ product }) => {
         </div>
         <figure className={styles['more-clickable-area']} onClick={() => handleClick(product)}>
           {state.cart.includes(product) ? (
-            <Image className={styles["disabled add-to-cart-btn"]} src={addedToCartImage} alt="added to cart" />
+            <Image className={styles["disabled add-to-cart-btn"]} src={addedToCartImage} alt="added to cart" width={50} height={50} />
           ) : (
             <Image className={styles["add-to-cart-btn pointer"]} src={addToCartImage} alt="add to cart" />
           )}
